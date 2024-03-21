18459a1d4c7df74ca6122a89a8df376195b267ea
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-03-21 23:10:53 +0000

    Update UserAccessService.php

--- UserAccessService.php
+++ UserAccessService.php
@@ -10,12 +10,16 @@ require_once("src/LoginHtmlTagNames.php");
 		<?php
 		if (isset($_POST[LoginHtmlTagNames::LoginButton]))
 		{
-			echo 'Login Button Pressed';
+			?>
+			<h1>Login Button Pressed</h1>
+			<?php
 			$username = $_POST[LoginHtmlTagNames::UsernameInput];
-			$username = $_POST[LoginHtmlTagNames::UsernameInput];
-			echo $username;
+			$password = $_POST[LoginHtmlTagNames::PasswordInput];
+			?>
+			<h2>Username:<?php echo $username; ?></h2>
+			<h2>Password:<?php echo $password; ?></h2>
+			<?php
 		}
-		// put your code here
 		?>
     </body>
 </html>
