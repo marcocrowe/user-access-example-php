05c85a54c2f76f024504e1b25ba3d94a732bba75
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-03-21 23:10:53 +0000

    Post Example

---
+++ src/pagesupport/LoginHtmlTagNames.php
@@ -0,0 +1,9 @@
+<?php
+
+class LoginHtmlTagNames
+{
+	const FormActionPage = "UserAccessService.php";
+	const LoginButton = "LoginButton";
+	const PasswordInput = "PasswordInput";
+	const UsernameInput = "ClientUsernameInput";
+}
