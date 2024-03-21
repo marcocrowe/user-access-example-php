297430c0241a1bb8446541a81ad8fb2d8818eafc
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-03-21 23:10:55 +0000

    A single file that contains all require_once calls for *.php files in the app directory And sets strict mode declare(strict_types=1);

---
+++ app/require_once.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+//
+//	app/AccessManagement/
+//
+require_once 'app/AccessManagement/SessionKeys.php';
+require_once 'app/AccessManagement/UserAccountLoginResult.php';
+require_once 'app/AccessManagement/UserSessionManagement.php';
+//
+//	app/DTOs/
+//
+require_once 'app/DTOs/UserAccount.php';
+//
+//	app/Repository/
+//
+require_once 'app/Repository/UserAccountRepository.php';
+//
+//	app/Repository.Database.Schema/
+//
+require_once 'app/Repository.Database.Schema/DatabaseSchema.php';
+//
+//	app/Repository.MySQL/
+//
+require_once 'app/Repository.MySQL/UserAccountMySQLRepository.php';
+//
+//	app/Repository.Session/
+//
+require_once 'app/Repository.Session/UserAccountSessionRepository.php';
+//
+//	app/Website/
+//
+require_once 'app/Website/MenuItem.php';
+require_once 'app/Website/WebPage.php';
+require_once 'app/Website/Website.php';
+//
+//	app/Website.Forms/
+//
+require_once 'app/Website.Forms/LoginHtmlTagNames.php';
+require_once 'app/Website.Forms/RegisterHtmlTagNames.php';
+require_once 'app/Website.Forms/UserAccountEditHtmlTagNames.php';
+require_once 'app/Website.Forms/UserAccountHtmlTagNames.php';
+//
+//	app/
+//
+require_once 'app/Application.php';
+require_once 'app/WebPages.php';
